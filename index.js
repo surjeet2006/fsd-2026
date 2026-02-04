3bd70ab5e57b45c58bb80fa6a7dcdca1794b09f0
surjeet2006 <vermasurjeetskv@gmail.com>
2026-02-04 15:55:37 +0530

    index.js

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ console.log(3+4)
 console.log("j")
 
 console.log("k")
+
+//date 
+let today=new Date();
+console.log(today);
+
+//Math
+console.log(Math.sqrt(25));
+console.log(Math.pow(2,5));
+console.log(Math.random());
+
+//String 
+txt="Hello world";
+console.log(txt.length);
+
+console.log(txt.includes("html"));
+
+//type convesion
+let strnum="123";
+let convert=Number(strnum);
+console.log(convert+10);
+
